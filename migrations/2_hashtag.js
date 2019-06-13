bdddc97ae3af8239c1b17478351a87072cb247eb
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2019-06-13 19:01:52 +0300

    Multiply blockIncome and totalSupply by 10^18

--- migrations/2_hashtag.js
+++ migrations/2_hashtag.js
@@ -1,5 +1,7 @@
 const Hashtag = artifacts.require("./Hashtag.sol");
 
 module.exports = async function(deployer) {
-    await deployer.deploy(Hashtag, "Highsup", "HUP", 18, 22500, 100000000);
+    const decimals = 18;
+    const multiplier = Math.pow(10, decimals);
+    await deployer.deploy(Hashtag, "Highsup", "HUP", decimals, 100 * multiplier, 100000000 * multiplier);
 };
